67193b59f91154d17bb464369135a479315e956a
WinStoker <31968363+WinStoker@users.noreply.github.com>
2020-04-23 13:48:44 -0400

    Changes

--- Leave-management/Controllers/LeaveTypesController.cs
+++ Leave-management/Controllers/LeaveTypesController.cs
@@ -35,7 +35,13 @@ namespace Leave_management.Controllers
         // GET: LeaveTypes/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            if (!_repo.isExists(id))
+            {
+                return NotFound();
+            }
+            var leavetype = _repo.FindById(id);
+            var model = _mapper.Map<LeaveTypeViewModel>(leavetype);            
+            return View(model);
         }
 
         // GET: LeaveTypes/Create
@@ -118,23 +124,42 @@ namespace Leave_management.Controllers
         // GET: LeaveTypes/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var leavetype = _repo.FindById(id);
+            if (leavetype == null)
+            {
+                return NotFound();
+            }
+            var isSuccess = _repo.Delete(leavetype);
+            if (!isSuccess)
+            {
+                return BadRequest();
+            }
+            return RedirectToAction(nameof(Index));
         }
 
         // POST: LeaveTypes/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, LeaveTypeViewModel model)
         {
             try
             {
                 // TODO: Add delete logic here
-
+                var leavetype = _repo.FindById(id);
+                if (leavetype == null)
+                {
+                    return NotFound();
+                }
+                var isSuccess = _repo.Delete(leavetype);
+                if (!isSuccess)
+                {
+                    return View(model);
+                }
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(model);
             }
         }
     }
